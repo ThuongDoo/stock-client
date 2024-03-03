169cd93d45e3adeb2f54460969c9a432ba5028ce
ThuongDoo <domanhthuong20122002@egmail.com>
2024-03-03 18:43:15 +0700

    commit

--- src/constants/socket.js
+++ src/constants/socket.js
@@ -1,2 +1,2 @@
-// export const SOCKET_SERVER_URL = "https://chungkhoanxyz.com/backend";
-export const SOCKET_SERVER_URL = "http://localhost:3000";
+export const SOCKET_SERVER_URL = "https://chungkhoanxyz.com/backend";
+// export const SOCKET_SERVER_URL = "http://localhost:3000";
