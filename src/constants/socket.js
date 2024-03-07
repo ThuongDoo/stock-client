62d744afe69e75c516169d5ea496037c42fd4fd6
ThuongDoo <domanhthuong20122002@egmail.com>
2024-03-07 07:42:11 +0700

    change header

--- src/constants/socket.js
+++ src/constants/socket.js
@@ -1,2 +1,2 @@
-// export const SOCKET_SERVER_URL = "https://chungkhoanxyz.com";
-export const SOCKET_SERVER_URL = "http://localhost:3000";
+export const SOCKET_SERVER_URL = "https://chungkhoanxyz.com";
+// export const SOCKET_SERVER_URL = "http://localhost:3000";
