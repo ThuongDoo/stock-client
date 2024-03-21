8d203ae88db251ff53a9eaf1587ec223d4d660e5
ThuongDoo <domanhthuong20122002@egmail.com>
2024-03-21 13:01:29 +0700

    update redux

--- src/utils/store.js
+++ src/utils/store.js
@@ -1,10 +1,25 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import themeSlice from "../slices/themeSlice";
 import userSlice from "../slices/userSlice";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 
-export default configureStore({
-  reducer: {
-    theme: themeSlice,
-    user: userSlice,
-  },
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const rootReducer = combineReducers({
+  theme: themeSlice,
+  user: userSlice,
 });
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const store = configureStore({
+  reducer: persistedReducer,
+});
+
+const persistor = persistStore(store);
+
+export { store, persistor };
