ccea8a077b849a27db48b0888ae760e5093c70ac
ThuongDoo <domanhthuong20122002@egmail.com>
2024-03-21 13:11:54 +0700

    update ui

--- src/layouts/Contact.jsx
+++ src/layouts/Contact.jsx
@@ -31,7 +31,7 @@ function Contact() {
         alt=""
         className=" absolute left-0 top-0 w-full h-full opacity-40 z-0"
       />
-      <div className=" z-10 w-full flex flex-col items-center justify-center gap-y-3">
+      <div className=" z-10 w-full flex flex-col items-center justify-center gap-y-3 p-4">
         <div>
           <h1 className=" text-5xl text-white ">Liên hệ tư vấn</h1>
         </div>
