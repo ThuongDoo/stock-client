c63485184a5accf6b8f20af85e9eb3334667badc
ThuongDoo <domanhthuong20122002@egmail.com>
2024-04-01 21:25:24 +0700

    fix filter

--- src/components/CustomGridTest.jsx
+++ src/components/CustomGridTest.jsx
@@ -4,26 +4,7 @@ import { DataGrid } from "@mui/x-data-grid";
 export default function CustomGridTest({ data }) {
   return (
     <div style={{ height: 400, width: "100%", backgroundColor: "white" }}>
-      <DataGrid
-        {...data}
-        // initialState={{
-        //   columns: {
-        //     columnVisibilityModel: {
-        //       // Hide columns status and traderName, the other columns will remain visible
-        //       RSRating: false,
-        //       "RS-O'neil": false,
-        //       RSI: false,
-        //       ADX: false,
-        //       "DMI ": false,
-        //       "DMI-": false,
-        //     },
-        //   },
-        //   pagination: { paginationModel: { pageSize: 5 } },
-        // }}
-        // pageSizeOptions={[5, 10, 25]}
-        // disableColumnFilter
-        // disableColumnMenu
-      />
+      <DataGrid columns={data.columns} rows={data.rows} />
     </div>
   );
 }
